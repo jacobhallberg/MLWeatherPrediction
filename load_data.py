187c9cacca4cb7fa9bbcf478cf2974d1c86be31e
jhallbe <jhallbe@sandia.gov>
2018-10-27 11:30:46 -0600

    Added class weighting support.

--- load_data.py
+++ load_data.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 # from keras.utils import to_categorical
 from sklearn.preprocessing import LabelEncoder
-
+from sklearn.utils import class_weight
 
 def load_data(data_location):
     return pd.read_csv(data_location)
@@ -19,12 +19,14 @@ def load_normalized_data(data_location, y_variable):
     # Matrix Math is easier with floats instead of strings. Convert String Values to encoded labels.
     le = LabelEncoder()
     le.fit(y_df)
-    y_df = pd.DataFrame(le.transform(y_df))
+    y_df = le.transform(y_df)
+    class_weights = class_weight.compute_class_weight('balanced',np.unique(y_df),y_df)
+    
 #     y_df = pd.DataFrame(to_categorical(y_df, len(np.unique(y_df, return_counts=True)[0])))
     
     
     normalized_X_df = min_max_normalization(X_df)
 
-    x_train, x_test, y_train, y_test = train_test_split(normalized_X_df, y_df, test_size=0.2)
+    x_train, x_test, y_train, y_test = train_test_split(normalized_X_df.values, y_df, test_size=0.2)
     
-    return x_train.values, x_test.values, y_train.values, y_test.values, y_df.nunique().values[0]
+    return x_train, x_test, y_train, y_test, np.unique(y_df).shape[0], class_weights
